d0c812245ffc6c8d34ceb61d36d1834ba69887a8
Felipe Abe <abe.fydr@gmail.com>
2025-01-02 17:13:29 -0300

    Added new fields for automation upload

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -8,16 +8,32 @@ describe('Orange HRM Test', () => {
     loginButton: "[type='submit']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredentialAlert: "[role='alert']"
+    wrongCredentialAlert: "[role='alert']",
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    middleNameField: "[name='middleName']",
+    lastNameField: "[name='lastName']",
+    genericField: ".oxd-input",
+    dateField: "[placeholder='yyyy-dd-mm']",
   }
 
-  it('Login - Success', () => {
+  it.only('User Info Update - Success', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get(selectorsList.dashboardGrid)
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('A')
+    cy.get(selectorsList.middleNameField).clear().type('A')
+    cy.get(selectorsList.lastNameField).clear().type('A')
+    cy.get(selectorsList.genericField).eq(4).clear().type('EmployeeIdTest')
+    cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
+    cy.get(selectorsList.genericField).eq(6).clear().type('Driver License Number Test')
+    cy.get(selectorsList.genericField).eq(7).clear().type('2029-03-07')
+    cy.get(selectorsList.genericField).eq(9).clear().type('Test Field Test')
+
   })
   it('Login - Fail', () => {
     cy.visit ('/auth/login')
