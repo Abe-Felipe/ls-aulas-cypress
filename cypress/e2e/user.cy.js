cdb69e3c3d10531eecf93fac729167563fe18149
Felipe Abe <abe.fydr@gmail.com>
2025-01-04 02:08:54 -0300

    Added dropdown choices for automation

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -17,6 +17,9 @@ describe('Orange HRM Test', () => {
     dateField: "[placeholder='yyyy-dd-mm']",
     dateCloseButton: ".--close",
     submitButton: ".oxd-button",
+    nationalityButton: ".oxd-select-text",
+    maritalButton: ".oxd-select-text",
+    bloodTypeButton: ".oxd-select-text",
   }
 
   it.only('User Info Update - Success', () => {
@@ -30,14 +33,22 @@ describe('Orange HRM Test', () => {
     cy.get(selectorsList.firstNameField).clear().type('Isso')
     cy.get(selectorsList.middleNameField).clear().type('É um')
     cy.get(selectorsList.lastNameField).clear().type('Teste')
-    cy.get(selectorsList.genericField).eq(5).clear().type('55667788')
-    cy.get(selectorsList.genericField).eq(6).clear().type('OtherIdTest')
-    cy.get(selectorsList.genericField).eq(7).clear().type('DL55')
-    cy.get(selectorsList.genericField).eq(8).clear().type('2029-09-09')
+    cy.get(selectorsList.genericField).eq(4).clear().type('55667788')
+    cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
+    cy.get(selectorsList.genericField).eq(6).clear().type('DL55')
+    cy.get(selectorsList.genericField).eq(7).clear().type('2029-09-09')
     cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get(selectorsList.nationalityButton).eq(0).click({force: true})
+    cy.get('.oxd-select-dropdown > :nth-child(4)').click({force: true})
+    cy.get(selectorsList.maritalButton).eq(1).click({force: true})
+    cy.get('.oxd-select-dropdown > :nth-child(4)').click({force: true})
+    cy.get(selectorsList.bloodTypeButton).eq(2).click({force: true})
+    cy.get('.oxd-select-dropdown > :nth-child(5)').click({force: true})
+    cy.get(selectorsList.submitButton).eq(0).click({force: true})
     cy.get('.oxd-toast-close')
+
   })
+
   it('Login - Fail', () => {
     cy.visit ('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
