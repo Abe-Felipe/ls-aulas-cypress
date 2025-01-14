1e7862ac147d4889bb6266d76bbc5dbcf32ee85d
Felipe Abe <abe.fydr@gmail.com>
2025-01-14 00:01:46 -0300

    new update with dropdowns fills

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -2,49 +2,24 @@ import userData from '../fixtures/users/userData.json'
 import loginPage from'../pages/loginPage'
 import dboardPage from '../pages/dashboardPage'
 import menuPage from '../pages/menuPage'
+import myInfoPage from '../pages/myInfoPage'
 
 const LoginPage = new loginPage
 const DashboardPage = new dboardPage
 const MenuPage = new menuPage
+const MyInfoPage = new myInfoPage
 
 describe('Orange HRM Test', () => {
-
-  const selectorsList = {
-
-    firstNameField: "[name='firstName']",
-    middleNameField: "[name='middleName']",
-    lastNameField: "[name='lastName']",
-    genericField: ".oxd-input",
-    dateField: "[placeholder='yyyy-dd-mm']",
-    dateCloseButton: ".--close",
-    submitButton: ".oxd-button",
-    nationalityButton: ".oxd-select-text",
-    maritalButton: ".oxd-select-text",
-    bloodTypeButton: ".oxd-select-text",
-  }
 
   it.only('User Info Update - Success', () => {
     LoginPage.accessLoginPage()
     LoginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
     DashboardPage.checkDashboardPage()
     MenuPage.accessMyInfo()
-    cy.get(selectorsList.firstNameField).clear().type('Isso')
-    cy.get(selectorsList.middleNameField).clear().type('É um')
-    cy.get(selectorsList.lastNameField).clear().type('Teste')
-    cy.get(selectorsList.genericField).eq(4).clear().type('55667788')
-    cy.get(selectorsList.genericField).eq(5).clear().type('OtherIdTest')
-    cy.get(selectorsList.genericField).eq(6).clear().type('DL55')
-    cy.get(selectorsList.genericField).eq(7).clear().type('2029-09-09')
-    cy.get(selectorsList.dateCloseButton).click()
-    cy.get(selectorsList.nationalityButton).eq(0).click({force: true})
-    cy.get('.oxd-select-dropdown > :nth-child(4)').click({force: true})
-    cy.get(selectorsList.maritalButton).eq(1).click({force: true})
-    cy.get('.oxd-select-dropdown > :nth-child(4)').click({force: true})
-    cy.get(selectorsList.bloodTypeButton).eq(2).click({force: true})
-    cy.get('.oxd-select-dropdown > :nth-child(5)').click({force: true})
-    cy.get(selectorsList.submitButton).eq(0).click({force: true})
-    cy.get('.oxd-toast-close')
-
+    MyInfoPage.fillNames()
+    MyInfoPage.fillId()
+    MyInfoPage.fillDropdowns()
+    MyInfoPage.submitUpdate()
   })
 
   it('Login - Fail', () => {
--- cypress/pages/myInfoPage.js
+++ cypress/pages/myInfoPage.js
@@ -1,11 +1,48 @@
 class myInfoPage {
 
-    selectorsList(){
+    selectorsList() {
             const selectors = {
-
+                firstNameField: "[name='firstName']",
+                middleNameField: "[name='middleName']",
+                lastNameField: "[name='lastName']",
+                genericField: ".oxd-input",
+                dateField: "[placeholder='yyyy-dd-mm']",
+                dateCloseButton: ".--close",
+                submitButton: ".oxd-button",
+                nationalityButton: ".oxd-select-text",
+                maritalButton: ".oxd-select-text",
+                bloodTypeButton: ".oxd-select-text",
             }
 
     return selectors
+    }
+
+    fillNames() {
+        cy.get(this.selectorsList.firstNameField).clear().type('Isso')
+        cy.get(this.selectorsList.middleNameField).clear().type('É um')
+        cy.get(this.selectorsList.lastNameField).clear().type('Teste')
+    }
+
+    fillId() {
+        cy.get(this.selectorsList.genericField).eq(4).clear().type('55667788')
+        cy.get(this.selectorsList.genericField).eq(5).clear().type('OtherIdTest')
+        cy.get(this.selectorsList.genericField).eq(6).clear().type('DL55')
+    }
+
+    fillDropdowns() {
+        cy.get(this.selectorsList.genericField).eq(7).clear().type('2029-09-09')
+        cy.get(this.selectorsList.dateCloseButton).click()
+        cy.get(this.selectorsList.nationalityButton).eq(0).click({force: true})
+        cy.get('.oxd-select-dropdown > :nth-child(4)').click({force: true})
+        cy.get(this.selectorsList.maritalButton).eq(1).click({force: true})
+        cy.get('.oxd-select-dropdown > :nth-child(4)').click({force: true})
+        cy.get(this.selectorsList.bloodTypeButton).eq(2).click({force: true})
+        cy.get('.oxd-select-dropdown > :nth-child(5)').click({force: true})
+    }
+
+    submitUpdate() {
+        cy.get(this.selectorsList.submitButton).eq(0).click({force: true})
+        cy.get('.oxd-toast-close')
     }
 }
 
