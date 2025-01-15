0701b0c0e7d3903f6ec51f0efdb4e9533a2951f1
Felipe Abe <abe.fydr@gmail.com>
2025-01-15 13:57:04 -0300

    updated dashboard check on login.cy

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,16 @@
 import userData from '../fixtures/users/userData.json'
 import loginPage from'../pages/loginPage'
+import dboardPage from '../pages/dashboardPage'
 
 const LoginPage = new loginPage
-
+const DashboardPage = new dboardPage
 
 describe('Login Orange HRM Test', () => {
 
   it('User Info Update - Success', () => {
     LoginPage.accessLoginPage()
     LoginPage.loginWithAnyUser(userData.userSuccess.username, userData.userSuccess.password)
+    DashboardPage.checkDashboardPage()
   })
 
   it('Login - Fail', () => {
